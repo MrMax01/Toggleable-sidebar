9410701f3de1a14b82099995e859f0bc8571a09b
MrMax01 <massimo.mauro00@gmail.com>
2023-07-02 19:04:35 +0200

    adding slide animation

--- javascripts/toggle.js
+++ javascripts/toggle.js
@@ -1,3 +1,7 @@
 document.getElementById("logo-name").addEventListener("click", () => {
   document.getElementById("sidebar").classList.toggle("active");
 });
+
+document.getElementById("home-btn").addEventListener("click", () => {
+  document.getElementById("home-menu").classList.toggle("home-show");
+});
